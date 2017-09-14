6f842e8ce7f213fcfc61b47f961ec3ceff16de76
Chris Cheung <c33cheun@gmail.com>
2017-09-13 21:41:32 -0400

    Use synchronize to do sequential calls

--- index.js
+++ index.js
@@ -1,54 +1,78 @@
 #!/usr/bin/env node
 const url = require('url');
-var http = require('http');
-var fs = require('fs');
-var program = require('commander');
+const http = require('http');
+const fs = require('fs');
+const program = require('commander');
+
+// synchronize is a npm library for making async requests sequential
+const sync = require('synchronize');
+var fiber = sync.fiber;
+var await = sync.await;
+var defer = sync.defer;
+
 
 var downloadURL;
 var isParallel = false;
 const defaultFile = "384MB.jar";
 var destination = defaultFile;
 
-// process command line arguments from process.argv, using commander module from node
-program
-  .arguments('<url>')
-  .option('-p, --parallel', 'Download chunks in parallel instead of sequentially')
-  .option('-o, --overwrite <destination>', 'Write to custom destination file instead of default')
-  .action(function (urlOption) {
-    downloadURL = urlOption;
-  })
-  .parse(process.argv);
-
-// check if we have command line arguments, we always have 2 by default ['node', 'MultiGet.js'], output help if we have nothing and exit
-if (!process.argv.slice(2).length) {
-  program.help();
+// store the ranges for the html request header in this const object for easy reference
+const byteRanges = {
+  1: "bytes=0-1048575",
+  2: "bytes=1048576-2097151",
+  3: "bytes=2097152-3145727",
+  4: "bytes=3145728-4194303"
 }
 
-// if we are given command line options but not a download url, ouput error and exit
-if (typeof downloadURL === 'undefined') {
-   console.error('No URL to download from provided!');
-   process.exit(1);
-}
+main();
 
-// set options if present
-if (program.parallel) {
-  isParallel = true;
-}
+function main() {
+  // process command line arguments from process.argv, using commander module from node
+  program
+    .arguments('<url>')
+    .option('-p, --parallel', 'Download chunks in parallel instead of sequentially')
+    .option('-o, --overwrite <destination>', 'Write to custom destination file instead of default')
+    .action(function (urlOption) {
+      downloadURL = urlOption;
+    })
+    .parse(process.argv);
 
-if (program.overwrite) {
-  destination = program.overwrite;
-}
+  // check if we have command line arguments, we always have 2 by default ['node', 'MultiGet.js'], output help if we have nothing and exit
+  if (!process.argv.slice(2).length) {
+    program.help();
+  }
 
-console.log("Downloading from: " + downloadURL);
-console.log("Downloading file to: " + destination);
+  // if we are given command line options but not a download url, ouput error and exit
+  if (typeof downloadURL === 'undefined') {
+     console.error('No Source URL to download from provided!');
+     process.exit(1);
+  }
 
-checkFile(destination);
+  // set options if present
+  if (program.parallel) {
+    isParallel = true;
+  }
 
-if (isParallel) {
+  if (program.overwrite) {
+    destination = program.overwrite;
+  }
 
-} else {
-  console.log("Starting download...");
-  download1();
+  console.log("Downloading from: " + downloadURL);
+  console.log("Downloading file to: " + destination);
+
+  // check and delete already present destination file
+  checkFile(destination);
+
+  // create url object for use in http requests
+  downloadURL = url.parse(downloadURL);
+
+  if (isParallel) {
+
+  } else {
+    console.log("Starting download...");
+    downloadParts();
+  }
+
 }
 
 // FUNCTIONS
@@ -61,53 +85,58 @@ function checkFile(destination) {
       fs.unlink(destination);
     } else {
       //Show in red
-      console.log('File not found, so not deleting.');
+      console.log('Creating new file...');
     }
   });
 }
 
-function download1() {
-  download(downloadURL, destination, "bytes=0-1048575", download2);
+function downloadParts() {
+  // use synchronize library to defer async functions until they finish
+  try {
+    fiber(function () {
+      await(downloadSync(1, defer() ));
+      await(downloadSync(2, defer() ));
+      await(downloadSync(3, defer() ));
+      await(downloadSync(4, defer() ));
+      await(finishDownload());
+    })
+  } catch(err) {
+    console.log("Error: " + err);
+  }
 }
 
-function download2() {
-  download(downloadURL, destination, "bytes=1048576-2097151", download3);
+function finishDownload() {
+  console.log("Done.");
 }
 
-function download3() {
-  download(downloadURL, destination, "bytes=2097152-3145727", download4);
-}
-
-function download4() {
-  download(downloadURL, destination, "bytes=3145728-4194303");
-}
-
-function download(urlString, dest, range, cb) {
-  var urlObject = url.parse(urlString);
+function downloadSync(range, cb) {
   var options = {
-      hostname: urlObject.hostname,
-      port: urlObject.port,
-      path: urlObject.pathname,
+      hostname: downloadURL.hostname,
+      port: downloadURL.port,
+      path: downloadURL.pathname,
       method: 'GET',
       headers: {
           accept: 'application/json',
-          range: range
+          range: byteRanges[range]
       }
   };
 
   var request = http.get(options, function(response) {
+    // to hold chunks of data
     response.on("data", function(chunk) {
-      fs.appendFile(dest, chunk, function (err) {
-        if (err) throw err;
-      });
+      fs.appendFileSync(destination, chunk)
     });
-    console.log('Wrote to file: ' + range);
-    // run callback after
-    if (cb) {
-      cb();
-    }
+
+    response.on("end", function() {
+      console.log('Wrote to file: ' + byteRanges[range]);
+
+      if (cb) {
+        cb();
+      }
+    });
+
   }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    console.error(`Got error: ${err.message}`);
+    fs.unlink(destination); // Delete the file async. (But we don't check the result)
+    console.log(`Got error: ${err.message}`);
   });
-};
+}
